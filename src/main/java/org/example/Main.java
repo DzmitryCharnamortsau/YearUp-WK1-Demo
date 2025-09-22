9510e517f45445c2d936137032c47843a1a8584a
Dzmitry Charnamortsau <d.charnamortsau@gmail.com>
2025-09-22 14:39:47 -0700

    deleted a line

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,6 +5,5 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Programming is so cool!");
-        System.out.println("My name is Dzmitry Charnamortsau");
         }
     }
