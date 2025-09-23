15dc3a9b9473251bbe628d0b55bd0e850ed90a03
Dzmitry Charnamortsau <d.charnamortsau@gmail.com>
2025-09-23 10:28:42 -0700

    Add files via upload

---
+++ ShoppingList.java
@@ -0,0 +1,15 @@
+package com.pluralsight;
+
+public class ShoppingList {
+    public static void main(String[] args) {
+        System.out.println("Beer");
+        System.out.println("Milk");
+        System.out.println("Bread");
+        System.out.println("Chicken");
+        System.out.println("Cheese");
+        System.out.println("Eggs");
+        System.out.println("Tomatoes");
+        System.out.println("Bananas");
+        System.out.println("Oranges");
+    }
+}
